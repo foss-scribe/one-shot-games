6e2bb704df9985086a90eb4807e7cedc87517b5d
Chris Gardiner-Bill <cbill.uk@gmail.com>
2025-07-17 10:54:25 +1000

    disabling ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  ssr: true,
+  ssr: false,
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
